347042d4bff7db1ae039408a9bc048db93fa2b46
johnwwweissberg <johnwwweissberg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-01 11:37:19 +0000

    V1.0.9 for WooCommerce
    git-svn-id: https://plugins.svn.wordpress.org/print-science-designer/tags/1.0.9@1125276 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/function.js
@@ -0,0 +1,99 @@
+jQuery(document).ready(function($) {
+    
+	
+	jQuery.each(jQuery('.product_type_simple'), function() {
+         if(jQuery(this).html()=='Personalize'){
+		    jQuery(this).removeClass('add_to_cart_button').addClass('personalize');   
+		 }
+        });
+	  
+	jQuery('a.personalizep').click(function(){
+		jQuery(this).attr('href');
+		var host= jQuery("[name='host']").val();
+		var url 	=	host+jQuery(this).attr('href');
+		jQuery(this).bind('click'); 
+		jQuery('#popup_frame').attr('src',url);
+		//jQuery('#popup-wrapper').modalPopLite({openButton:'.personalizep',closeButton:'#close-btn', isModal: true });
+	  
+	});
+	jQuery('button.personalizep').click(function(){
+   	 var data_array=jQuery(".variations_form").serialize();
+	  var server_url= jQuery("[name='server_url']").val();	
+	  if(data_array==''){
+  	   var attcart= jQuery("[name='add-to-cart']").val();
+	   var quantity= jQuery("[name='quantity']").val();	
+	   data_array=  'add-to-cart='+attcart+'&quantity='+quantity;
+	 }
+	 //var url= server_url+''+data_array;
+         var url = "";
+         var strfind = server_url.indexOf('?');
+	 if(strfind > 0)
+         {
+             var querystr = url;
+             url = server_url + "&" + data_array;
+         }
+         else
+         {
+           url = server_url + "?" + data_array;  
+         }
+         //alert(url);
+         //alert(newstr);
+	 jQuery(this).bind('click'); 
+		jQuery('#popup_frame').attr('src',url);
+		//jQuery('#popup-wrapper').modalPopLite({openButton:'.personalizep',closeButton:'#close-btn', isModal: true });
+	   
+	});
+	
+	
+	//jQuery('.personalizep').trigger('click'); 
+	
+	//jQuery('.buttons_added:button').removeClass('single_add_to_cart_button');
+	//alert(jQuery('.cart .button').html());
+	 
+});
+function closethepopup(){
+            //alert(jQuery('#server_url').val());
+			window.parent.location.href=jQuery('#server_url').val();
+			//window.parent.location.reload(true);
+			jQuery('#popup_frame').attr("src", "");
+			//jQuery('#close-btn').trigger('click');
+	}
+	
+jQuery(function () {
+	 var maskWidth = document.body.clientWidth;
+	 var maskHeight = jQuery(window).height();
+     var margin =  jQuery("[name='margin']").val();
+     jQuery("#popup-wrapper").css("width",(maskWidth - (2*margin)));
+	 jQuery("#popup-wrapper").css("height",(maskHeight - 2*margin));
+	 jQuery("#popup_frame").css("width",(maskWidth - 2*margin));
+	 jQuery("#popup_frame").css("height",(maskHeight - 2*margin));	 
+	
+	jQuery("#popup-wrapper").modalPopLite({openButton:".personalizep",closeButton:"#close-btn", isModal: true });
+	 
+ });
+
+
+/*	jQuery(function (){
+	
+		var maskWidth = document.body.clientWidth;
+		var maskHeight = jQuery(window).height();
+		jQuery(".anchoreditlink").attr("target","popup_frame");
+		jQuery(".personalize_btn_link").attr("target","popup_frame");
+		jQuery('#popup-wrapper').modalPopLite({ openButton: '.personalizep', closeButton: '#close-btn', isModal: true });
+	});	
+	function closethepopup(){
+		window.top.location.reload();
+		
+		
+	}
+	function setPersonalizeLocation(formObj) {
+		jQuery("#popup-wrapper").css("display","block");
+		if (jQuery(formObj).length > 0)
+		{
+			jQuery(formObj).attr("target","popup_frame");
+			jQuery(formObj).submit();
+		}	
+		jQuery('.personalizep').trigger('click');
+		return false;
+	}
+*/
